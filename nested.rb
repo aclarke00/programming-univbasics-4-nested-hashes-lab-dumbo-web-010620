e8a2fc255c62d627906a8f7920abedffeedf03f3
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-12-29 02:50:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,6 +60,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     array = programmer_hash[:dennis_ritchie][:languages]
+		 array.join
 end
 
 def adding_matz
