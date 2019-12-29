7157c42700eb226223140ebef8c9f1de3b4073ec
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-12-29 02:47:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -41,7 +41,7 @@ def alan_kay_is_known_for
         }
         
      }
-     	return programmer_hash[:grace_hopper]
+     	 programmer_hash[:grace_hopper]
 end
 
 def dennis_ritchies_language
